7d81493ddd188f95788ba610bce98369ea251fe0
DoYouHaveWine <zhshofqian@163.com>
2020-12-20 00:28:13 +0800

    updata

---
+++ pages/login/login.js
@@ -0,0 +1,44 @@
+// pages/login/login.js
+Page({
+
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		codeMsg: '获取验证码',
+		loginCode: true
+	},
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function(options) {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function() {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function() {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function() {
+
+	},
+	switchLogin() {
+		this.setData({
+			loginCode: !this.data.loginCode
+		})
+	}
+})
