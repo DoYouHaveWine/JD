717b7723001fc7d183a70c91f39842d266b8eab9
DoYouHaveWine <zhshofqian@163.com>
2021-01-14 13:00:25 +0800

    updata

--- pages/scan/scan.js
+++ pages/scan/scan.js
@@ -1,66 +1,81 @@
 // pages/scan/scan.js
+import drawImg from '../../utils/qrcode.js'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		qrcodeURL: "",
+		codeText: "https://www.baidu.com",
+	},
 
-  },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function(options) {
+this.drawImgs();
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function() {
 
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function() {
 
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function() {
 
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function() {
 
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function() {
 
-  },
+	},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function() {
 
-  },
+	},
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function() {
 
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+	},
+	drawImgs: function () {
+	    console.log(this.data.codeText);
+			console.log(drawImg)
+	    let imgData = drawImg(this.data.codeText, {
+	      typeNumber: 4,
+	      errorCorrectLevel: 'M',
+	      size: 500
+	    })
+			console.log(imgData)
+	    this.setData({
+	      qrcodeURL: imgData
+	    })
+	  }
 })
